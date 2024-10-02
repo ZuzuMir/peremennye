7757d29f86676309b3d930060faf653ad2a3b730
ZuzuMir <laraminnullina0247@gmail.com>
2024-10-02 18:04:14 +0300

    Add files via upload

---
+++ dz peremennye.py
@@ -0,0 +1,6 @@
+tasks = 12
+hours = 1.5
+course = 'Python'
+time_for_one_task = hours / tasks
+print('Курс: ', course, ', всего задач: ', tasks, ', затрачено часов: ', hours, ', среднее время выполнения: ', time_for_one_task, '.' )
+print('Курс:' + str(course) + ',', 'всего задач:' + str(tasks) + ',', 'затрачено часов:' + str(hours) + ',', 'среднее время выполнения:' + str(time_for_one_task) + '.' )
